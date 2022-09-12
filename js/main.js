c7e2b99425b57408b0c58fd961bab1fd15c3240f
Noman Bin Basheer <nomanbinbasheer66626@gmail.com>
2022-09-12 23:39:14 +0500

    coded a calculator object

--- js/main.js
+++ js/main.js
@@ -1,15 +1,69 @@
-// document.querySelector('button').addEventListener('click', getFetch)
+// Required abillities of a calculator:
 
-function getFetch(userInput){
-  const choice = userInput
-  const url = `https://world.openfoodfacts.org/api/v0/product/${choice}.json`
+// should accept inputs of numbers, operators and another number
+// should accept decimal numbers
+// store inputs
+// recognize inputs and perform calculations 
+// returb a result
 
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+// Optional features:
+
+// should accept longer arithematic operations
+// display all inputs as it is being entered.
+// store previous total as start of next operation
+// clear button should clear all enteries
+// should prevent invalid inputs (e.g. operators next to each other, two decimal points)
+
+
+const keys = document.querySelector('.buttonContainer');
+
+keys.addEventListener('click', buttonPressed =>{
+     const {target} = buttonPressed
+     const {value}  = target
+     if (!target.matches('button')) {
+          return
+     }
+     else{
+          parseInput(value)
+     }
+     
+})
+
+const calculator = {
+     displayText: '0',
+     prevTotal: null,
+
+
+     parseInput(value){
+          if (this.displayText == '0'){
+               this.displayText = ''
+          }
+
+     //    Have any of the "special buttons" been clicked?
+
+      switch (value) {
+          case '=' :
+          //   calculate the answer
+               break;
+          case 'AC' : 
+          //  clear screen and all values
+               break;
+          case '.' :
+           if (this.displayText == '0'){
+               // pass '0.' to the text
+           }
+           else{
+               // add value (.) to the text string
+           }
+           break;
+
+           default: 
+          //  add value to the text string
+      }
+      
+      addText(value){
+          
+      }
+
+     }
 }
